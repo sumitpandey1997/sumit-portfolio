6ddf07d3d37e849b25787dfede92451ab925311f
sumit pandey <pandeysumit22121997@gmail.com>
2020-05-02 15:43:45 +0530

    Updates

---
+++ precache-manifest.810a26ee58ad3d798b0237bd3ee73b1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ae0ed930f8c7577a81aead371b5807",
+    "url": "/sumit-portfolio/index.html"
+  },
+  {
+    "revision": "d8ad2f2937599c15e461",
+    "url": "/sumit-portfolio/static/css/2.d2ca2446.chunk.css"
+  },
+  {
+    "revision": "44c741bf6f51258ddca6",
+    "url": "/sumit-portfolio/static/css/main.c825a9a1.chunk.css"
+  },
+  {
+    "revision": "d8ad2f2937599c15e461",
+    "url": "/sumit-portfolio/static/js/2.fd52966d.chunk.js"
+  },
+  {
+    "revision": "44c741bf6f51258ddca6",
+    "url": "/sumit-portfolio/static/js/main.587abc5d.chunk.js"
+  },
+  {
+    "revision": "4826e348b6b2869abc50",
+    "url": "/sumit-portfolio/static/js/runtime-main.77592943.js"
+  },
+  {
+    "revision": "39aae8b2596b55fb3b3e596e76e76ee3",
+    "url": "/sumit-portfolio/static/media/android.39aae8b2.jpeg"
+  },
+  {
+    "revision": "17281b46599e686ff5e7593f66cbae93",
+    "url": "/sumit-portfolio/static/media/dl.17281b46.jpg"
+  },
+  {
+    "revision": "5bdfb650e619dc66c389c42e286517ac",
+    "url": "/sumit-portfolio/static/media/front.5bdfb650.jpeg"
+  },
+  {
+    "revision": "1ee5351dd6308db8c3bb527d74e20dbd",
+    "url": "/sumit-portfolio/static/media/java.1ee5351d.jpg"
+  },
+  {
+    "revision": "e0b4d364af52c5b99f662585ff318d61",
+    "url": "/sumit-portfolio/static/media/ml.e0b4d364.jpeg"
+  },
+  {
+    "revision": "13dbac35095d66721010011f7811cb4b",
+    "url": "/sumit-portfolio/static/media/profile.13dbac35.jpeg"
+  },
+  {
+    "revision": "ffda00ca8dd693156b39efafe20403aa",
+    "url": "/sumit-portfolio/static/media/profile.ffda00ca.jpg"
+  }
+]);
